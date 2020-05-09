d66647bae1e7d81f472cf52af28442f490b55da8
yanwei.han <yanwei.han@okcoin.net>
2020-05-09 09:00:09 +0800

    singleton pattern

---
+++ src/headfirst/designpatterns/singleton/chocolate/ChocolateBoiler.java
@@ -0,0 +1,50 @@
+package headfirst.designpatterns.singleton.chocolate;
+
+public class ChocolateBoiler {
+  private static volatile ChocolateBoiler uniqueInstance;
+  private boolean empty;
+  private boolean boiled;
+
+  private ChocolateBoiler() {
+    empty = true;
+    boiled = false;
+  }
+
+  public static ChocolateBoiler getInstance() {
+    if (uniqueInstance == null) {
+      synchronized (ChocolateBoiler.class) {
+        if (uniqueInstance == null) {
+          uniqueInstance = new ChocolateBoiler();
+        }
+      }
+    }
+    return uniqueInstance;
+  }
+
+  public void fill() {
+    if (isEmpty()) {
+      empty = false;
+      boiled = false;
+    }
+  }
+
+  public void drain() {
+    if (!isEmpty() && isBoiled()) {
+      empty = true;
+    }
+  }
+
+  public void boil() {
+    if (!isEmpty() && !isBoiled()) {
+      boiled = true;
+    }
+  }
+
+  public boolean isEmpty() {
+    return empty;
+  }
+
+  public boolean isBoiled() {
+    return boiled;
+  }
+}
